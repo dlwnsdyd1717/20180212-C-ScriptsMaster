379dc92c6d3837e058a7bf41a7c6eff4edcde947
dlwnsdyd1717 <36186758+dlwnsdyd1717@users.noreply.github.com>
2018-02-19 18:13:24 +0900

    20180219 - json
    a

--- Assets/scrpits/EnemyManager.cs
+++ Assets/scrpits/EnemyManager.cs
@@ -20,33 +20,33 @@ public class EnemyManager : MonoBehaviour {
 		
 	}
 	void Update () {
-        //Respawncool += Time.deltaTime;
-        //if (Respawncool > Respawndelay)
-        //{
-        //    enemyCnt++;
-        //    GameObject enemyInstantiate = Instantiate(enemy) as GameObject;
-        //    enemyInstantiate.name = "Enemy" + enemyCnt;
-        //    enemyInstantiate.transform.position = RespawnPoint.position;
-        //    enemyInstantiate.GetComponent<CrowdAgent>().target = GameObject.FindGameObjectWithTag("EnemyPoint").transform;
-        //    Respawncool = 0;
-        //}
-        //if(EnemyTrigger.Score > 100)
-        //{
-        //    RespawCool2 += Time.deltaTime;
-        //    if (RespawCool2 > Respawndelay - 0.5f)
-        //    {
-        //        enemy2cnt++;
-        //        GameObject enemy2Instatiate = Instantiate(Enemy2) as GameObject;
-        //        enemy2Instatiate.name = "Enemy2" + enemy2cnt;
-        //        enemy2Instatiate.transform.position = RespawnPoint.position + new Vector3(0,0,1);
-        //        enemy2Instatiate.GetComponent<CrowdAgent>().target = GameObject.FindGameObjectWithTag("EnemyPoint").transform;
-        //        RespawCool2 = 0;
-        //    }
-        //}
-        //if(EnemyTrigger.Score > 200)
-        //{
+        Respawncool += Time.deltaTime;
+        if (Respawncool > Respawndelay)
+        {
+            enemyCnt++;
+            GameObject enemyInstantiate = Instantiate(enemy) as GameObject;
+            enemyInstantiate.name = "Enemy" + enemyCnt;
+            enemyInstantiate.transform.position = RespawnPoint.position;
+            enemyInstantiate.GetComponent<CrowdAgent>().target = GameObject.FindGameObjectWithTag("EnemyPoint").transform;
+            Respawncool = 0;
+        }
+        if (EnemyTrigger.Score > 100)
+        {
+            RespawCool2 += Time.deltaTime;
+            if (RespawCool2 > Respawndelay - 0.5f)
+            {
+                enemy2cnt++;
+                GameObject enemy2Instatiate = Instantiate(Enemy2) as GameObject;
+                enemy2Instatiate.name = "Enemy2" + enemy2cnt;
+                enemy2Instatiate.transform.position = RespawnPoint.position + new Vector3(0, 0, 1);
+                enemy2Instatiate.GetComponent<CrowdAgent>().target = GameObject.FindGameObjectWithTag("EnemyPoint").transform;
+                RespawCool2 = 0;
+            }
+        }
+        if (EnemyTrigger.Score > 200)
+        {
             RespawnCool3 += Time.deltaTime;
-            if(RespawnCool3 > Respawndelay + 1)
+            if (RespawnCool3 > Respawndelay + 1)
             {
                 enemy3cnt++;
                 GameObject enemy3Instatiate = Instantiate(Enemy3) as GameObject;
@@ -55,6 +55,6 @@ public class EnemyManager : MonoBehaviour {
                 enemy3Instatiate.GetComponent<CrowdAgent>().target = GameObject.FindGameObjectWithTag("EnemyPoint").transform;
                 RespawnCool3 = 0;
             }
-        //}	
-	}
+        }
+    }
 }
